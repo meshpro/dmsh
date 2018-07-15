17eb592d4b0d60a158352917952325ad2434d97e
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-15 19:04:12 +0200

    fix initial triangulation

--- dmsh/main.py
+++ dmsh/main.py
@@ -22,7 +22,7 @@ def generate():
     bb_width = geo.bounding_box[1] - geo.bounding_box[0]
     bb_height = geo.bounding_box[3] - geo.bounding_box[2]
     num_steps_x = int(bb_width / h0) + 1
-    num_steps_y = int(bb_height / h0) + 1
+    num_steps_y = int(bb_height / (h0 * numpy.sqrt(3) / 2)) + 1
 
     # Generate initial (staggered) point list from bounding box
     x, y = numpy.meshgrid(
@@ -33,27 +33,37 @@ def generate():
     x[1::2] += h0 / 2
     pts = numpy.array([x.reshape(-1), y.reshape(-1)])
 
+    eps = 1.0e-10
+
     # remove points outside of the region
-    pts = pts[:, geo.isinside(pts) < 0.0]
+    pts = pts[:, geo.isinside(pts) < eps]
 
     # evaluate the element size function, remove points according to it
     p = element_size_function(pts)
     pts = pts[:, numpy.random.rand(pts.shape[1]) < p]
 
+    # import matplotlib.pyplot as plt
+    # t = numpy.linspace(0.0, 2 * numpy.pi, 100)
+    # plt.plot(numpy.cos(t), numpy.sin(t), '-')
+    # plt.plot(pts[0], pts[1], '.')
+    # plt.axis("square")
+    # plt.show()
+    # exit(1)
+
     while True:
         if True:
             # Re-Delauneyfication
-            pts_old = pts.copy()
+            # pts_old = pts.copy()
 
             # compute Delaunay triangulation
             tri = scipy.spatial.Delaunay(pts.T)
             cells = tri.simplices.copy()
 
-            # kick out all cells whose barycenter is not in the region
+            # kick out all cells whose barycenter is not in the geometry
             bc = numpy.sum(pts[:, cells], axis=-1) / 3.0
             cells = cells[geo.isinside(bc) < 0.0]
 
-            # Find edges
+            # Determine edges
             edges = numpy.concatenate(
                 [cells[:, [0, 1]], cells[:, [1, 2]], cells[:, [2, 0]]]
             )
@@ -64,6 +74,7 @@ def generate():
 
         edges_vec = pts[:, edges[:, 1]] - pts[:, edges[:, 0]]
         edge_lengths = numpy.sqrt(edges_vec[0] ** 2 + edges_vec[1] ** 2)
+        edges_vec /= edge_lengths
 
         # Evaluate element sizes at edge midpoints
         edge_midpoints = (pts[:, edges[:, 1]] + pts[:, edges[:, 0]]) / 2
@@ -80,14 +91,13 @@ def generate():
         force_abs[force_abs < 0.0] = 0.0
 
         # force vectors
-        force = force_abs * (edges_vec / edge_lengths)
+        force = force_abs * edges_vec
 
         force_per_node = numpy.zeros(pts.T.shape)
-        fastfunc.add.at(force_per_node, edges[:, 0], -force)
-        fastfunc.add.at(force_per_node, edges[:, 1], +force)
-        force_per_node = force_per_node.T
+        fastfunc.add.at(force_per_node, edges[:, 0], +force)
+        fastfunc.add.at(force_per_node, edges[:, 1], -force)
 
-        pts = pts + delta_t * force_per_node
+        pts = pts + delta_t * force_per_node.T
 
         # show(pts, cells, geo)
 
