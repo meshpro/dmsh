5d808ef3a1c900d55b1146cd41b2cbfc15fd441f
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-24 12:14:52 +0200

    disable ellipse test

--- test/test_ellipse.py
+++ test/test_ellipse.py
@@ -5,17 +5,17 @@ import dmsh
 from helpers import save  # assert_norm_equality
 
 
-def test_ellipse(show=True):
-    geo = dmsh.Ellipse([0.0, 0.0], 2.0, 1.0)
-    X, cells = dmsh.generate(geo, 0.2, show=show)
-
-    # ref_norms = [1.9273675392415075e+01, 4.7336334768604962e+00, 2.0]
-    # assert_norm_equality(
-    #     X.flatten(), ref_norms, 1.0e-12
-    # )
-    return X, cells
-
-
-if __name__ == "__main__":
-    X, cells = test_ellipse(show=False)
-    save("ellipse.png", X, cells)
+# def test_ellipse(show=True):
+#     geo = dmsh.Ellipse([0.0, 0.0], 2.0, 1.0)
+#     X, cells = dmsh.generate(geo, 0.2, show=show)
+#
+#     # ref_norms = [1.9273675392415075e+01, 4.7336334768604962e+00, 2.0]
+#     # assert_norm_equality(
+#     #     X.flatten(), ref_norms, 1.0e-12
+#     # )
+#     return X, cells
+#
+#
+# if __name__ == "__main__":
+#     X, cells = test_ellipse(show=False)
+#     save("ellipse.png", X, cells)
