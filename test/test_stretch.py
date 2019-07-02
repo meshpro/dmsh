ea3b118ba65ebdeb1a4f800817288110bdbe9cd3
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-02 10:48:32 +0200

    black

--- test/test_stretch.py
+++ test/test_stretch.py
@@ -7,7 +7,7 @@ def test(show=True):
     geo = dmsh.Stretch(dmsh.Rectangle(-1.0, +2.0, -1.0, +1.0), [1.0, 1.0])
     X, cells = dmsh.generate(geo, 0.2, show=show, tol=1.0e-3)
 
-    ref_norms = [4.3336958624757887e+02, 2.3620906390718797e+01, 2.6213203435596428e+00]
+    ref_norms = [4.3336958624757887e02, 2.3620906390718797e01, 2.6213203435596428e00]
     assert_norm_equality(X.flatten(), ref_norms, 1.0e-12)
     return X, cells
 
