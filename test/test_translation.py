088239b55164f500a463bfbf36f589d777cb17c8
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-17 09:56:48 +0200

    relax test tolerance

--- test/test_translation.py
+++ test/test_translation.py
@@ -7,7 +7,7 @@ def test(show=False):
     X, cells = dmsh.generate(geo, 0.1, show=show)
 
     ref_norms = [1.7525e03, 5.5677444529404333e01, 3.0]
-    assert_norm_equality(X.flatten(), ref_norms, 1.0e-10)
+    assert_norm_equality(X.flatten(), ref_norms, 1.0e-7)
 
 
 if __name__ == "__main__":
