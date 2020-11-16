45de5618cef97f8b64c8c20dec978b909bae5066
Nico Schlömer <nico.schloemer@gmail.com>
2020-11-16 13:10:17 +0100

    merge intersection tests

--- test/test_intersection.py
+++ test/test_intersection.py
@@ -17,6 +17,22 @@ def test_intersection(show=False):
     return X, cells
 
 
+def test_intersection_circles(show=False):
+    angles = numpy.pi * numpy.array([3.0 / 6.0, 7.0 / 6.0, 11.0 / 6.0])
+    geo = dmsh.Intersection(
+        [
+            dmsh.Circle([numpy.cos(angles[0]), numpy.sin(angles[0])], 1.5),
+            dmsh.Circle([numpy.cos(angles[1]), numpy.sin(angles[1])], 1.5),
+            dmsh.Circle([numpy.cos(angles[2]), numpy.sin(angles[2])], 1.5),
+        ]
+    )
+    X, cells = dmsh.generate(geo, 0.1, show=show, tol=1.0e-10)
+
+    ref_norms = [6.7650924332929776e01, 5.0559320825183374e00, 7.2474487138537913e-01]
+    assert_norm_equality(X.flatten(), ref_norms, 1.0e-10)
+    return X, cells
+
+
 def test_boundary_step():
     geo = dmsh.Intersection(
         [dmsh.Circle([0.0, -0.5], 1.0), dmsh.Circle([0.0, +0.5], 1.0)]
